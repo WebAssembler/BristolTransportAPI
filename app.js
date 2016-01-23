d10e301e7ace0700303deeeedaddc36fa62eda84
Anthony Hartnell <anthony@schoolforstartups.net>
2016-01-23 23:52:32 +0000

    update api with marker animation

--- app.js
+++ app.js
@@ -1,114 +1,87 @@
-function initMap() {
-    var myLatLng = {lat: 10, lng: -2.617999};
-
-    var map = new google.maps.Map(document.getElementById('map'), {
-        // zoom: 14,
-        // center: myLatLng
-
-    });
-
-
-}
-
 $( document ).ready(function() {
-    var myKey = 'YOUR_API_KEY';
+    var apiKey = "&apikey=MY-API-KEY";
+        baseUrl = "https://bristol.api.urbanthings.io/api/2.0/static/",
+        routeX38 = "trips?routeID=UK_TNDS_NOC_FBRI-X39&includePolylines=true" + apiKey,
+        transitions = "transitstops?stopIDs=";
 
-    var staticUrl = "https://bristol.api.urbanthings.io/api/2.0/static/";
-
-    var routeX38 = "trips?routeID=UK_TNDS_NOC_FBRI-X39&includePolylines=true" + myKey;
-    var transitions = "transitstops?stopIDs=";
+    // Make Initial API Request
     $.ajax({
-    url: staticUrl + routeX38,
-    type: "GET",
+        url: baseUrl + routeX38,
+        type: "GET",
 
-    // first ajax
-    success: function(data){
+        success: function(data) {
+            console.log(data);
 
-        console.log(data);
-        var myLatLng = {lat: 51.443506, lng: -2.617999};
+            // Set up map and options
+            var map = new google.maps.Map(document.getElementById('map'), {
+                zoom: 12,
+                center: {lat: 51.417861, lng: -2.501804}
+            });
 
-        var myMap = new google.maps.Map(document.getElementById('map'), {
-            zoom: 14,
-            center: myLatLng
-        });
+            // Grab polyline from json and escape backslashes
+            var escapedPolyline = data.data[0].polyline.replace(/\\/g,"\\\\");
 
-        // find replace polyline
-        var escapedLine = data.data[0].polyline.replace(/\\/g,"\\\\");
+            // Encode back into array of objects
+            var polylineCoords = google.maps.geometry.encoding.decodePath(escapedPolyline);
 
-        // decode polyline to array
-        var myPath = google.maps.geometry.encoding.decodePath(escapedLine);
-
-        var myPathArray = [];
-        for(var i = 0; i < myPath.length; i++) {
-            //console.log(myPath[i].lat(), myPath[i].lng());
-            myPathArray.push({ lat: myPath[i].lat(), lng: myPath[i].lng() });
-        }
-
-        var busPath = new google.maps.Polyline({
-            path: myPathArray,
-            strokeColor: '#00AAEE',
-            strokeOpacity: 1.0,
-            strokeWeight: 2
-        });
-
-
-        // get all stop calls and store in array
-        var stopsArray = [];
-        for(var a = 0; a < data.data[0].stopCalls.length; a++) {
-            stopsArray.push(data.data[0].stopCalls[a].transitStopPrimaryCode);
-        }
-
-        // ajax inception
-        $.ajax({
-            url: staticUrl + transitions + stopsArray.join() + myKey,
-            type: "GET",
-
-            success: function(stops) {
-
-                var customStops = [];
-                for(var b = 0; b < stops.data.length; b++) {
-                    customStops.push({ lat: stops.data[b].lat, lng:stops.data[b].lng});
-                }
-
-                //Do the PolyLines
-                // var customStopsPath = new google.maps.Polyline({
-                //     path: customStops,
-                //     geodesic: true,
-                //     strokeColor: '#ff0000',
-                //     strokeOpacity: 1.0,
-                //     strokeWeight: 2
-                // });
-                // customStopsPath.setMap(myMap);
-
-                // // Do the Markers
-                for(var g=0; g < customStops.length; g++){
-                   var marker = new google.maps.Marker({
-                	   position: customStops[g],
-                	   map: myMap
-                	 });
-                //
-                }
-            },
-            error: function(a){
-                console.log(a);
+            // Loop through coords to create array of just lat long
+            var routePolyline = [];
+            for(var i = 0; i < polylineCoords.length; i++) {
+                routePolyline.push({ lat: polylineCoords[i].lat(), lng: polylineCoords[i].lng() });
             }
-        });
 
+            // Plot this polyline onto map
+            var plottedRoute = new google.maps.Polyline({
+                path: routePolyline,
+                strokeColor: '#00AAEE',
+                strokeOpacity: 1.0,
+                strokeWeight: 2
+            });
 
+            // get all stop calls and store in array
+            var stopsArray = [];
+            for(var i = 0; i < data.data[0].stopCalls.length; i++) {
+                stopsArray.push(data.data[0].stopCalls[i].transitStopPrimaryCode);
+            }
 
+            // ajax inception
+            $.ajax({
+                url: baseUrl + transitions + stopsArray.join() + apiKey,
+                type: "GET",
 
-        //console.log(myPathArray);
-        // Plot a dodgy map
-        //busPath.setMap(myMap);
+                success: function(stops) {
 
+                    // Plot the custom stops
+                    var customStops = [];
+                    for(var b = 0; b < stops.data.length; b++) {
+                        customStops.push({ lat: stops.data[b].lat, lng: stops.data[b].lng});
+                    }
 
+                    // Animate the Markers on the map
+                    for(var i = 0; i < customStops.length; i++) {
+                        addMarkerWithTimeout(customStops[i], i * 100);
+                    }
 
-    },
-    error: function(a){
-        console.log(a);
-    }
+                    // Draw the markers
+                    var markers = [];
+                    function addMarkerWithTimeout(position, timeout) {
+                      window.setTimeout(function() {
+                        markers.push(new google.maps.Marker({
+                          position: position,
+                          map: map,
+                          animation: google.maps.Animation.DROP
+                        }));
+                      }, timeout);
+                    }
+
+                },
+                error: function(a) {
+                    console.log(a);
+                }
+            });
+        },
+        error: function(a){
+            console.log(a);
+        }
     })
-
-
-
 });
